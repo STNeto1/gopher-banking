c77c38534d4b9d26ce7a563b49073904e440c87b
STNeto1 <gfreitasneto18@gmail.com>
2023-02-24 22:45:53 -0300

    refactor user extraction

--- web/pkg/auth/controller.go
+++ web/pkg/auth/controller.go
@@ -1,9 +1,14 @@
 package auth
 
 import (
+	"context"
 	"core/auth"
+	"errors"
+	"fmt"
+	"models/ent"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type handler struct {
@@ -19,4 +24,28 @@ func RegisterRoutes(r *gin.Engine, service *auth.AuthService) {
 	routes.GET("/profile", h.Profile)
 	routes.POST("/login", h.Login)
 	routes.POST("/register", h.Register)
+}
+
+func (h handler) ExtractUser(raw interface{}) (*ent.User, error) {
+	valStr := fmt.Sprint(raw)
+	if valStr == "" {
+		return nil, errors.New("")
+	}
+
+	claims, err := h.service.ValidateToken(valStr)
+	if err != nil {
+		return nil, errors.New("")
+	}
+
+	parsedUserId, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return nil, errors.New("")
+	}
+
+	usr, err := h.service.GetUserFromId(context.Background(), parsedUserId)
+	if err != nil {
+		return nil, errors.New("")
+	}
+
+	return usr, nil
 }
--- web/pkg/auth/profile.go
+++ web/pkg/auth/profile.go
@@ -1,38 +1,17 @@
 package auth
 
 import (
-	"fmt"
 	"net/http"
 	"web/pkg/common/exceptions"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 )
 
 func (h handler) Profile(c *gin.Context) {
 	session := sessions.Default(c)
 
-	rawToken := session.Get("token")
-	valStr := fmt.Sprint(rawToken)
-	if valStr == "" {
-		c.JSON(http.StatusInternalServerError, exceptions.Unauthorized("unauthorized"))
-		return
-	}
-
-	claims, err := h.service.ValidateToken(valStr)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, exceptions.Unauthorized("unauthorized"))
-		return
-	}
-
-	parsedUserId, err := uuid.Parse(claims.Subject)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, exceptions.Unauthorized("unauthorized"))
-		return
-	}
-
-	usr, err := h.service.GetUserFromId(c.Request.Context(), parsedUserId)
+	usr, err := h.ExtractUser(session.Get("token"))
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, exceptions.Unauthorized("unauthorized"))
 		return
